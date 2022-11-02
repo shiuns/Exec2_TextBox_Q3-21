a553d0737f8c5ad50b682543607d2fcc60b89588
shiuns <shiunworks@gmail.com>
2022-11-02 22:49:53 +0800

    加入專案檔案。

---
+++ [作業02-Exec2_TextBox_Q3]21謝孟勳/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _作業02_Exec2_TextBox_Q3_21謝孟勳
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                DateTime input;
+                try
+                {
+                    input = DateReal();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                string result = age(input);
+                MessageBox.Show(result);
+            }
+
+        }
+
+        private DateTime DateReal()
+        {
+            string input = textBox1.Text;
+            bool isDate = DateTime.TryParse(input, out DateTime Day);
+
+            if (isDate == false)
+            {
+                throw new Exception("格式錯誤");
+            }
+            return Day;
+        }
+
+        private string age(DateTime input)
+        {
+            string result;
+            int NowYear = DateTime.Now.Year;
+            int inputyear = input.Year;
+
+            if (NowYear - inputyear >= 13)
+            {
+                result = "已滿13歲";
+            }
+            else
+            {
+                result = "未滿13歲";
+            }
+            return result;
+        }
+    }
+}
